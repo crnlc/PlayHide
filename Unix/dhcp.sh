2170025fc2efea40a357c95402dc2232675bb16b
DoM!niC <info@dom1nic.eu>
2018-06-06 05:48:05 +0200

    Unix Client/Config's for OpenVPN added

---
+++ Unix/dhcp.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# 
+[ -x /sbin/resolvconf ] || exit 0
+[ "$script_type" ] || exit 0
+[ "$dev" ] || exit 0
+
+split_into_parts()
+{
+	part1="$1"
+	part2="$2"
+	part3="$3"
+}
+
+[ -x /sbin/dhclient ] || exit 0
+
+case $script_type in
+
+up)
+        dhclient -v tap3
+	NMSRVRS=""
+	SRCHS=""
+	for optionvarname in ${!foreign_option_*} ; do
+		option="${!optionvarname}"
+		echo "$option"
+		split_into_parts $option
+		if [ "$part1" = "dhcp-option" ] ; then
+			if [ "$part2" = "DNS" ] ; then
+				NMSRVRS="${NMSRVRS:+$NMSRVRS }$part3"
+			elif [ "$part2" = "DOMAIN" ] ; then
+				SRCHS="${SRCHS:+$SRCHS }$part3"
+			fi
+		fi
+	done
+	R=""
+	[ "$SRCHS" ] && R="search $SRCHS
+"
+	for NS in $NMSRVRS ; do
+        	R="${R}nameserver $NS
+"
+	done
+	echo -n "$R" | /sbin/resolvconf -a "${dev}.openvpn"
+
+        ;;
+down)
+        dhclient -r tap3
+	/sbin/resolvconf -d "${dev}.openvpn"
+        ;;
+esac
---
+++ Unix/playhide_connect.sh
@@ -0,0 +1,1 @@
+screen -d -S PlayHide-VPN -m openvpn /etc/openvpn/playhide.conf
