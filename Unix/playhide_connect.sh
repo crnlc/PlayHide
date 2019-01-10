432235078137ae9c07de2ff91f7bd44f62f17535
DoM!niC <info@dom1nic.eu>
2019-01-10 20:21:24 +0100

    Start Commands and Fixes in Unix Script

--- Unix/playhide_connect.sh
+++ Unix/playhide_connect.sh
@@ -1,32 +1,46 @@
-chmod 744 ./ -R
 LoginTXT="./config/login.txt"
 MACTXT="./config/MAC.txt"
+MAC=cat $MACTXT &> /dev/null
 
-if [ ! -f $LoginTXT ];
+if [ "$1" = "setup" ];
 then
 echo "Setup PlayHide VPN"
+chmod 744 ./ -R
+rm $LoginTXT
 apt install -y openvpn isc-dhcp-client
 touch $LoginTXT
 head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 >> $LoginTXT
 echo -e "" >> $LoginTXT
 head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 >> $LoginTXT
 dhclient -r tap3 &> /dev/null
-pkill openvpn &> /dev/null
-screen -d -S PlayHide-VPN -m openvpn config/playhide.conf
-sleep 3
-touch $MACTXT
-cat /sys/class/net/tap3/address > $MACTXT
-sleep 1
-screen -X -S PlayHide-VPN quit
-pkill openvpn &> /dev/null
+pkill -f "openvpn --daemon --config ./config/playhide.conf" &> /dev/null
+#openvpn --config ./config/playhide.conf &> /dev/null
+#touch $MACTXT
+#cat /sys/class/net/tap3/address > $MACTXT
+pkill -f "openvpn --config ./config/playhide.conf" &> /dev/null
 echo "Done! Run Script again!"
-else
-echo "StartUp PlayHide VPN"
-MAC=cat $MACTXT &> /dev/null
-screen -X -S PlayHide-VPN quit &> /dev/null
+fi
+
+if [ "$1" = "start" ];
+then
+echo "Start PlayHide VPN"
+pkill -f "openvpn --daemon --config ./config/playhide.conf" &> /dev/null
 dhclient -r tap3 &> /dev/null
-screen -d -S PlayHide-VPN -m openvpn config/playhide.conf --lladdr $MAC
+sleep 1
+openvpn --daemon --config ./config/playhide.conf &> /dev/null
 sleep 2
 dhclient -v tap3 &> /dev/null
 ip addr|awk '/tap3/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print "Your IP is : " $2}'
+fi
+
+if [ "$1" = "status" ];
+then
+ip addr|awk '/tap3/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print "Your IP is : " $2}'
+fi
+
+if [ "$1" = "stop" ];
+then
+echo "Stop PlayHide VPN"
+dhclient -r tap3 &> /dev/null
+pkill -f "openvpn --daemon --config ./config/playhide.conf" &> /dev/null
 fi
